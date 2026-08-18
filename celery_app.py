1aef7bb7262c42a9f9ff996c5a042a598f157fd4
kiran-kumari-BM <kirankumarimanjunath@gmail.com>
2026-08-18 21:49:45 +0530

    Use environment Redis URL for Celery

--- celery_app.py
+++ celery_app.py
@@ -1,9 +1,19 @@
+import os
+
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_URL = os.environ.get(
+    "REDIS_URL",
+    "redis://127.0.0.1:6379/0"
+)
 
 celery = Celery(
     "doc_ai",
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0"
+    broker=REDIS_URL,
+    backend=REDIS_URL
 )
 
 celery.conf.update(
@@ -11,5 +21,5 @@ celery.conf.update(
     accept_content=["json"],
     result_serializer="json",
     timezone="Asia/Kolkata",
-    enable_utc=False,
+    enable_utc=False
 )
